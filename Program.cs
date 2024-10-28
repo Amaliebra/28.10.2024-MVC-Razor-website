a936921c18ae013d652ba1ae29e0b6928781edcd
Amaliebra <jba082024@gmail.com>
2024-10-28 12:25:22 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace _28._10._2024_MVC_WEBSITE;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
