6bdc10fef9c3bb6466c0e51ee31597e9faac1839
Amaliebra <amaliebratland@gmail.com>
2024-10-28 17:04:13 +0100

    added more setup

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -5,8 +5,16 @@ namespace Microsoft.AspNetCore.Controllers;
 
 public class MainController : Controller
 {
+    private readonly List<BlogPost> _blogPosts = new();
     public IActionResult Index()
     {
-        return View();
+        if (!_blogPosts.Any())
+        {
+            _blogPosts.Add(new BlogPost { Title = "Heia bloggen!", Content = "Dette er mitt første blogginnlegg! Håper dere gleder dere til neste oppdatering." });
+        }
+
+        return View(_blogPosts);
     }
+
+
 }
--- Models/Models.cs
+++ Models/Models.cs
@@ -1,1 +1,9 @@
 namespace MVC_Website.Models;
+
+public class BlogPost
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public
+}
